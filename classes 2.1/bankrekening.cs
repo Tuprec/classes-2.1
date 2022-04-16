87e06528d094f1d5af6016586a3fc7e7a4ce14a6
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-04-16 15:26:22 +0200

    class

--- classes 2.1/bankrekening.cs
+++ classes 2.1/bankrekening.cs
@@ -6,5 +6,30 @@ namespace classes_2._1
 {
     class bankrekening
     {
+        private int intrekeningstand;
+        private int intlimiet;
+        private int intverrichtingen;
+
+        public int RStand
+        {
+            get { return intrekeningstand; }
+            set { intrekeningstand = value; }
+        }
+        public int Limiet
+        {
+            get { return intlimiet; }
+            set { intlimiet = value; }
+        }
+        public int Verrichting
+        {
+            get { return intverrichtingen; }
+            set { intverrichtingen = value; }
+        }
+        public bankrekening()
+        {
+            RStand = 0;
+            Limiet = 0;
+            Verrichting = 0;
+        }
     }
 }
