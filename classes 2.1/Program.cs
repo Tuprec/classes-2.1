42aba5e2f1547fb09513b9ae6d189306ba49190f
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-04-16 15:43:03 +0200

    basis

--- classes 2.1/Program.cs
+++ classes 2.1/Program.cs
@@ -6,7 +6,51 @@ namespace classes_2._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bankrekening rekening = new bankrekening();
+            string strAnswer;
+            int intKeuze;
+            LimietSet(rekening);
+            Gegevens(rekening);
+            do
+            {
+                Console.Write("Wat Wilt u doen? \n1. Storting \n2. Afhalen");
+                intKeuze = Convert.ToInt32(Console.ReadLine());
+                if (intKeuze == 1)
+                {
+                    Storting(rekening);
+                }
+                else
+                {
+                    Afhalen(rekening);
+                }
+                
+               
+                Console.WriteLine("Wilt u nog een transactie uitvoeren? ");
+                strAnswer = Console.ReadLine().ToUpper();
+            } while (strAnswer == "Y");
+        }
+
+        private static void Afhalen(bankrekening rekening)
+        {
+            
+        }
+
+        private static void Storting(bankrekening rekening)
+        {
+            
+        }
+
+        private static void Gegevens(bankrekening rekening)
+        {
+            Console.WriteLine($"Er staat {rekening.RStand} Euro op uw rekening.");
+            Console.WriteLine($"U mag {rekening.Limiet} Euro onder 0 gaan.");
+            Console.WriteLine($"U heeft {rekening.Verrichting} transacties gedaan.");
+            Console.ReadLine();
+        }
+
+        private static void LimietSet(bankrekening rekening)
+        {
+            rekening.Limiet = -1000;
         }
     }
 }
